b1e435ab905aa65004cbd3c24a664c3f75dc57f1
imeanimakemonyhere-cpu <imeanimakemonyhere@gmail.com>
2025-09-28 18:53:53 +0300

    Add files via upload

--- python.py
+++ python.py
@@ -1,34 +1,35 @@
-def calculator():
-    while True:
-        print("\n***Calculator***")
-        print("1. addition (+)")
-        print("2. substraction (-)")
-        print("3. multiplication (*)")
-        print("4. division (/)")
-        print("5. Exit ")
-        choice = input("Enter Your choice (1-5): ")
+import random
+import string
 
-        if choice == "5":
-           print("Eixitting...")
-           break
 
-        num1 = float(input("Enter the first number: "))
-        num2 = float(input("Enter the second number: "))
-        if choice == "1":
-            print(f"result : {num1 + num2}")
-        elif choice == "2":
-            print(f"result : {num1 - num2}")
-        elif choice == "3":
-            print(f"result : {num1 * num2}")
-        elif choice == "4":
-            if num1 != 0:
-                print(f"result : {num1 / num2}")
-            else:
-                print("Error... cannot divide by zero")
-        else:
-            print("invalid choice please try (1-5)")
+chars = string.whitespace + string.punctuation + string.digits + string.ascii_letters
+chars = list(chars) 
+key = chars.copy()
 
-calculator()
-print()
+random.shuffle(key)
 
+while True:
+    plain_text = input("Enter a message to encrypt (q to quit): ")
+    if plain_text.upper() == "Q":
+        print("GOODBYE....")
+        break
+    cipher_text = ""
+
+    for letter in plain_text:
+        index = chars.index(letter)
+        cipher_text += key[index]
+    
+    print(f"Original message : {plain_text}")
+    print(f"Original message : {cipher_text}")
+
+    cipher_text = input("Enter a message to dencrypt: ")
+    plain_text = ""
+
+    for letter in cipher_text:
+        index = key.index(letter)
+        plain_text += chars[index]
+    
+    print(f"Original message : {cipher_text}")
+    print(f"Original message : {plain_text}")
+    
 
